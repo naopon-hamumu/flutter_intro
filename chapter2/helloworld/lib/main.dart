0d28d054e5c79aa4ce92f9f959e6488ab5c25709
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2024-03-30 16:20:37 +0900

    step2 2行目に文字列を表示する

--- chapter2/helloworld/lib/main.dart
+++ chapter2/helloworld/lib/main.dart
@@ -40,7 +40,10 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Text("HelloWorld"),
+      body: Column(children:[
+        const Text("HelloWorld"),
+        const Text("ハローワールド"),
+      ])
     );
   }
 }

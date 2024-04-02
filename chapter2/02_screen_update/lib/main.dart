b80d35431e91b9e916d00cd82e063d302b29814a
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2024-04-02 15:24:10 +0900

    title change

---
+++ chapter2/02_screen_update/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+// 1. エントリーポイントのmain関数
+void main() {
+  // 2. MyAppを呼び出す
+  runApp(const MyApp());
+}
+
+// MyAppのクラス
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    // 3.タイトルとテーマを設定する。画面の本体はMyHomePageで作る。
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  // 5.カウンタが押された時のメソッド
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      print("HelloWorld");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 4.MyHomePageの画面を構成する部分
+    return Scaffold(
+      // 画面上部のタイトル部分
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // 画面の中央に表示されるテキスト
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            // テキストの下に表示されるカウンタ値
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      // 右下の「+」ボタンに対応するフローティングアクションボタン
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+}

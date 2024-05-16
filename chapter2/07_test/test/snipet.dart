63968f8cfb94db5b3c51dd9d0748bbe658c9c2da
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2024-05-16 22:25:24 +0900

    事前処理・事後処理

--- chapter2/07_test/test/snipet.dart
+++ chapter2/07_test/test/snipet.dart
@@ -1,7 +1,17 @@
 import 'package:flutter_test/flutter_test.dart';
 
+// void main() {
+//   test('.parse() fails on invalid input', () {
+//     expect(() => int.parse('x'), throwsFormatException);
+//   });
+// }
+
 void main() {
-  test('.parse() fails on invalid input', () {
-    expect(() => int.parse('x'), throwsFormatException);
-  });
+  setUpAll(() {print("テスト全体の事前処理");});
+  setUp(() {print("メソッドごとの事前処理");});
+  tearDown(() {print("メソッドごとの事後処理");});
+  tearDownAll(() {print("テスト全体の事後処理");});
+  // 以後テストケースを記述
+  test("mytest1", () {print("mytest1");});
+  test("mytest2", () {print("mytest2");});
 }

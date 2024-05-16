b56af7a7bec5e1553c78f95dd3a4397b7383fbab
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2024-05-16 18:40:26 +0900

    initial commit

---
+++ chapter2/06_page_transition/page_view/lib/test_page1.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TestPage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test1"),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () => {
+            Navigator.of(context).pushNamed("/test2")
+            // 下記の書き方でも可
+            // Navigator.pushNamed(context, "/test2")
+          },
+        child: const Text("進む", style: TextStyle(fontSize: 80)))));
+  }
+}
---
+++ chapter2/06_page_transition/page_view/lib/test_page2.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class TestPage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Test2"),
+      ),
+      body: Center(
+        child:
+          Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            TextButton(
+              onPressed: () => {
+                Navigator.of(context).pushNamed("/test3")
+              },
+              child: const Text("進む", style: TextStyle(fontSize: 80))),
+            TextButton(
+              onPressed: () => {
+                Navigator.of(context).pushNamed("/test1")
+              },
+              child: const Text("戻る", style: TextStyle(fontSize: 80)))
+          ])));
+  }
+}
